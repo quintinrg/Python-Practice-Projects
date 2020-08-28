683269ee22381136918dd07a0c2fb4119887aaff
quintinrg <57852403+quintinrg@users.noreply.github.com>
2020-08-27 19:55:20 -0500

    Update guessing_game_auto.py
    Refactoring

--- guessing_game_auto.py
+++ guessing_game_auto.py
@@ -2,15 +2,16 @@
 import random
 import time
 
-my_rand_int = random.randint(0,100)
-number_of_guesses = 0 
-top_limit = 101
-bottom_limit = 0 
+bottom_limit = 0
+top_limit = 101 #change this to set the upper range for the game.
+my_rand_int = random.randint(0, (top_limit - 1))
 guess = ''
-print(f'Welcome to the guessing game! \nThe machine will guess a number between {bottom_limit} and {top_limit - 1}')
+number_of_guesses = 0 
+
+print(f'Welcome to the guessing game! \nThe machine will guess a number from {bottom_limit} to {top_limit - 1}')
 
 while guess != my_rand_int: 
-  time.sleep(0.5)
+  time.sleep(0.5) #Change this decimal to change speed of program, for human viewer
   guess = (bottom_limit + top_limit) / 2
   guess = int(guess)
   print(f'Guessing {guess}')
@@ -27,6 +28,8 @@ while guess != my_rand_int:
     print(f' Attempt number: {number_of_guesses}')
     
   elif guess == my_rand_int:
-    print(f'Correct number found! It was {my_rand_int}! \nIt took {number_of_guesses} tries.')
-
-
+    if number_of_guesses == 1:
+      try_tries = "try"
+    else:
+      try_tries = "tries"
+    print(f'Correct number found! It was {my_rand_int}! \nIt took {number_of_guesses} {try_tries}.')
