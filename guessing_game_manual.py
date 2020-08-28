6ea9d482ddb9bb7e6b22079942b953609268b556
quintinrg <57852403+quintinrg@users.noreply.github.com>
2020-08-27 20:02:56 -0500

    Update guessing_game_manual.py
    Added conditional text for guessing in one try.

--- guessing_game_manual.py
+++ guessing_game_manual.py
@@ -1,12 +1,13 @@
 # Manual version of Guessing Game
 import random
 
-my_rand_int = random.randint(0,100)
+bottom_limit = 0
+top_limit = 100
+my_rand_int = random.randint(bottom_limit, top_limit)
 number_of_guesses = 0 
-top_limit = 101
-bottom_limit = 0 
+ 
 guess = ''
-print(f'Welcome to the guessing game! \nGuess a number between {bottom_limit} and {top_limit - 1}')
+print(f'Welcome to the guessing game! \nGuess a number from {bottom_limit} to {top_limit - 1}')
 
 while guess != my_rand_int: 
   guess = input(f'Please enter your guess: ')
@@ -20,16 +21,16 @@ while guess != my_rand_int:
   number_of_guesses = number_of_guesses + 1
 
   if guess > my_rand_int:   
-    top_limit = guess
     print('Your guess was too high.') 
     print(f' Attempt number: {number_of_guesses}')
 
   elif guess < my_rand_int: 
-    bottom_limit = guess
     print('Your guess was too small.')
     print(f' Attempt number: {number_of_guesses}')
     
   elif guess == my_rand_int:
-    print(f'You win! The number was {my_rand_int}! \nIt took you {number_of_guesses} tries.')
-
-
+    if guess == 1:
+      try_tries = "try"
+    else:
+      try_tries = "tries"
+    print(f'You win! The number was {my_rand_int}! \nIt took you {number_of_guesses} {try_tries}.')
